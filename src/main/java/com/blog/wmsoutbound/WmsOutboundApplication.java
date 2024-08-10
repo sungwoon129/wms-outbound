ea726ceee85aace7293892cdfa6cc1f73c88bc04
master <sungwoony@kakao.com>
2024-08-10 13:59:52 +0900

    Initial commit

---
+++ src/main/java/com/blog/wmsoutbound/WmsOutboundApplication.java
@@ -0,0 +1,13 @@
+package com.blog.wmsoutbound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsOutboundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmsOutboundApplication.class, args);
+    }
+
+}
